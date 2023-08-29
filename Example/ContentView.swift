779e63bdd18dbfbc13ce7407fd7e62f63a9532d2
Radek Novak <radek.novis@gmail.com>
2023-08-29 16:12:34 +0200

    Cleanup

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -7,7 +7,6 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text(HelloWorld.greeting())
         }
         .padding()
     }
--- Web3Modal/Sources/Web3Modal/HelloWorld.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public struct HelloWorld {
-    
-    public static func greeting() -> String {
-        "Hello world from Web3Modal"
-    }
-}
