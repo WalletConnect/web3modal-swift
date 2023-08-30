ef9aa1a6e84dae0d9d50977e0aae4129b545485f
Radek Novak <radek.novis@gmail.com>
2023-08-30 16:26:57 +0200

    Add W3M button with snapshot tests

---
+++ Web3Modal/Sources/Web3Modal/Resources/Extensions/Font+extension.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+extension Font {
+    static var large500: Font = .system(size: 20.0).weight(.regular)
+    static var large600: Font = .system(size: 20.0).weight(.medium)
+    static var large700: Font = .system(size: 20.0).weight(.semibold)
+    static var micro600: Font = .system(size: 10.0).weight(.medium)
+    static var micro700: Font = .system(size: 10.0).weight(.semibold)
+    static var paragraph500: Font = .system(size: 16.0).weight(.regular)
+    static var paragraph600: Font = .system(size: 16.0).weight(.medium)
+    static var paragraph700: Font = .system(size: 16.0).weight(.semibold)
+    static var small500: Font = .system(size: 14.0).weight(.regular)
+    static var small600: Font = .system(size: 14.0).weight(.medium)
+    static var tiny500: Font = .system(size: 12.0).weight(.regular)
+    static var tiny600: Font = .system(size: 12.0).weight(.medium)
+    static var title500: Font = .system(size: 24.0).weight(.regular)
+    static var title600: Font = .system(size: 24.0).weight(.medium)
+    static var title700: Font = .system(size: 24.0).weight(.semibold)
+}
