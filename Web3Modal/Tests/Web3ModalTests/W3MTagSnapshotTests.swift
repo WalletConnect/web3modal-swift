643507efe8d6da4f46b1681cc2425a5fe58819cc
Radek Novak <radek.novis@gmail.com>
2023-09-13 14:01:40 +0200

    Fix tag snapshots

--- Web3Modal/Tests/Web3ModalTests/W3MTagSnapshotTests.swift
+++ Web3Modal/Tests/Web3ModalTests/W3MTagSnapshotTests.swift
@@ -7,7 +7,7 @@ final class W3MTagSnapshotTests: XCTestCase {
     
     func test_snapshots() throws {
         let view = W3MTagPreviewView()
-        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 100, height: 250), traits: .init(userInterfaceStyle: .dark)))
-        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 100, height: 250), traits: .init(userInterfaceStyle: .light)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 150, height: 250), traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 150, height: 250), traits: .init(userInterfaceStyle: .light)))
     }
 }
