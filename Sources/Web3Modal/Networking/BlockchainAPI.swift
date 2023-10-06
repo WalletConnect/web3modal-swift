b4183faaa664090e28fe2a23bb4976630e5a2aee
Radek Novak <radek.novis@gmail.com>
2023-10-06 16:40:34 +0200

    Introduce Blockchain API &  update interactors

---
+++ Sources/Web3Modal/Networking/BlockchainAPI.swift
@@ -0,0 +1,46 @@
+import Foundation
+import HTTPClient
+
+enum BlockchainAPI: HTTPService {
+    struct GetIdentityParams {
+        let address: String
+        let chainId: String
+        let projectId: String
+    }
+    
+    case getIdentity(params: GetIdentityParams)
+
+    var path: String {
+        switch self {
+        case let .getIdentity(params): return "/v1/identity/\(params.address)"
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .getIdentity: return .get
+        }
+    }
+
+    var body: Data? {
+        nil
+    }
+
+    var queryParameters: [String: String]? {
+        switch self {
+        case let .getIdentity(params):
+            return [
+                "projectId": params.projectId,
+                "chainId": params.chainId,
+            ]
+        }
+    }
+
+    var scheme: String {
+        return "https"
+    }
+
+    var additionalHeaderFields: [String: String]? {
+        nil
+    }
+}
