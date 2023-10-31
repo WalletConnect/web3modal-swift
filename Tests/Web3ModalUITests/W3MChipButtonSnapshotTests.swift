e68c1b1560264965ca13608db44294d301c6404f
Radek Novak <radek.novis@gmail.com>
2023-10-31 15:27:55 +0100

    Fix snapshot tests

--- Tests/Web3ModalUITests/W3MChipButtonSnapshotTests.swift
+++ Tests/Web3ModalUITests/W3MChipButtonSnapshotTests.swift
@@ -7,7 +7,7 @@ final class W3MChipButtonSnapshotTests: XCTestCase {
     
     func test_snapshots() throws {
         let view = W3MChipButtonStylePreviewView()
-        assertSnapshot(matching: view, as: .image(layout: .device(config: .iPhone13), traits: .init(userInterfaceStyle: .dark)))
-        assertSnapshot(matching: view, as: .image(layout: .device(config: .iPhone13), traits: .init(userInterfaceStyle: .light)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 800, height: 800), traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 800, height: 800), traits: .init(userInterfaceStyle: .light)))
     }
 }
