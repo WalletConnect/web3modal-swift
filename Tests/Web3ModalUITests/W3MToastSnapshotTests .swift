ef9734b4749adf6ad3b333f1f4a917b3fa6c555f
Radek Novak <radek.novis@gmail.com>
2023-11-10 18:36:20 +0100

    Record snapshots

--- Tests/Web3ModalUITests/W3MToastSnapshotTests .swift
+++ Tests/Web3ModalUITests/W3MToastSnapshotTests .swift
@@ -7,7 +7,7 @@ final class W3MToastSnapshotTests: XCTestCase {
     
     func test_snapshots() throws {
         let view = ToastViewPreviewView()
-        assertSnapshot(matching: view, as: .image(layout: .device(config: .iPhone13), traits: .init(userInterfaceStyle: .dark)))
-        assertSnapshot(matching: view, as: .image(layout: .device(config: .iPhone13), traits: .init(userInterfaceStyle: .light)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 250, height: 250), traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(matching: view, as: .image(layout: .fixed(width: 250, height: 250), traits: .init(userInterfaceStyle: .light)))
     }
 }
